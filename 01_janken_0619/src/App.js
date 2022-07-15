08be743790ce7216cebe9b9ddaef61b8a66e9060
ryugah <108250709+ryugah@users.noreply.github.com>
2022-07-15 22:39:41 +0900

    じゃんけんアプリ

---
+++ 01_janken_0619/src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      player_hand: null,
+      isSubmitted: false,
+      results: {}
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    if (this.state.player_hand === null) {
+      this.setState({ error: "手を選択してください" })
+      return
+    }
+
+    this.setState({ error: null })
+    this.setState({ isSubmitted: true })
+
+    // axios.post(API_URL, {
+    //   hand: this.state.player_hand
+    // }).then(res => {
+    //   this.setState({ results: res.data.body })
+    // })
+  }
+
+  onClick(e, value) {
+    this.setState({ player_hand: value })
+    Array.prototype.slice.call(
+      document.getElementsByClassName("active")
+    ).forEach(
+      function (node) {
+        node.classList.remove("active")
+      }
+    );
+    e.currentTarget.classList.add("active")
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>じゃんけんアプリ</h1>
+        </header>
+        <div id="button-area">
+          <div className="hand-button" onClick={(e) => this.onClick(e, 0)}>グー</div>
+          <div className="hand-button" onClick={(e) => this.onClick(e, 1)}>チョキ</div>
+          <div className="hand-button" onClick={(e) => this.onClick(e, 2)}>パー</div>
+        </div>
+        <div id="error">{this.state.error}</div>
+        {this.state.isSubmitted &&
+          <div id="result">
+            <p>
+              CPUの出した手は…<br />
+
+            </p>
+            <p>
+              {this.state.results.result}
+            </p>
+          </div>
+        }
+        <form onSubmit={this.onSubmit}>
+          <button type="submit" className="submit-button">勝負</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
